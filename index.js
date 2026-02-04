c2b7ae4c0141cf754c99b6d82ea7428a02850c72
deepanshu572 <142653242+deepanshu572@users.noreply.github.com>
2026-02-04 12:40:38 +0530

    fifth commit

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import express from "express";
-import cors from "cors"
+import cors from "cors";
 import authRouter from "./routes/authRoute.js";
 const app = express();
 import cookieParser from "cookie-parser";
@@ -11,20 +11,20 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
 app.use(
   cors({
-    origin: "https://vercel-frontend-5irfhs9y7-deepanshu-kumars-projects.vercel.app",
+    origin: [
+      "http://localhost:5173",
+      "https://vercel-frontend-5irfhs9y7-deepanshu-kumars-projects.vercel.app",
+    ],
     credentials: true,
-  })
+  }),
 );
+
 app.use("/api/auth", authRouter);
 app.use("/api/project", projectRouter);
 app.use("/api/task", taskRouter);
 
-app.listen(8080, async () => {
-  console.log("server started");
-  await connectDb();
-});
+await connectDb();
 
-
+export default app;
